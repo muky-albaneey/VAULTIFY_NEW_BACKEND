09aa7164eabcdec6330c9288988ce2d26beba6c7
Mozibix <opuzmoses@gmail.com>
2025-10-28 11:55:40 +0100

    chores>: updated

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -3,7 +3,7 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { JwtService } from '@nestjs/jwt';
 import { ConfigService } from '@nestjs/config';
-import * as bcrypt from 'bcrypt';
+import * as bcrypt from 'bcryptjs';
 import { User, UserStatus } from '../../entities/user.entity';
 import { JwtPayload, RefreshTokenPayload } from '../../common/interfaces/common.interface';
 import { EmailService } from '../../common/services/email.service';
